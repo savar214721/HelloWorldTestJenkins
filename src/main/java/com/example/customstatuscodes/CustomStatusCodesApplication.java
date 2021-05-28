2148782bb9836feba186317fa93f82c3cc334410
savar214721 <66008640+savar214721@users.noreply.github.com>
2021-05-28 12:43:22 +0530

    CustomStatusCodeAgainChanged

--- src/main/java/com/example/customstatuscodes/CustomStatusCodesApplication.java
+++ src/main/java/com/example/customstatuscodes/CustomStatusCodesApplication.java
@@ -2,6 +2,8 @@ package com.example.customstatuscodes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -10,12 +12,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class CustomStatusCodesApplication {
+public class CustomStatusCodesApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomStatusCodesApplication.class, args);
     }
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder  application){
+        return application.sources(CustomStatusCodesApplication.class);
+    }
     @Bean
     public Docket productApi(){
         return new Docket(DocumentationType.SWAGGER_2).select()
